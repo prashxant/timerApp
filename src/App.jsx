2de3d87da4a4de7cb2f550d7ad2fb2637aa807c0
prashxant <notxdead0@gmail.com>
2025-04-06 18:50:06 +0530

    ahhh!!1

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
+import React, { useEffect } from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const [play,setPlay] = useState(false)
+const [count,setCount]= useState(0)
+
+function handleClick(){
+  setPlay( (c)=>!c)
+ }
+
+  useEffect(() => {
+    if (!play) return;
+    const interval = setInterval(() => {
+      setCount((c) => c + 1);
+
+    },1000);
+    return () => clearInterval(interval);
+  },[play])
+
+
+function notok(){
+  return(
+    <div>
+      <h2>Not Ok</h2>
+    </div>
+  )}
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <> 
+   <div>{play? count: notok()}</div>
+   <button onClick={handleClick}>
+    {play? "Pause" : "Play"}
+   </button>
+   </>
   )
 }
 
