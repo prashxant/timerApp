4e7628d353372f1142752114dcd94b64db5dde49
prashxant <notxdead0@gmail.com>
2025-04-12 18:37:41 +0530

    gari thok ki aaaj

--- src/Counterr.jsx
+++ src/Counterr.jsx
@@ -6,23 +6,45 @@ import React, { useState } from 'react'
 
 function Counterr() {
 
-    const [count,setCount] = useState(0)
-
-    function Inc (){
-     setCount((c)=> c+1)
-    }
-    
-    function Dic (){
-        setCount((c)=> c-1)
-    }
-    
   return (
     <div>
-        <button onClick={Inc}>Increse</button>
-        <button onClick={Dic}>Decrease</button>
-        <div>{count}</div>
+       <Countt/>
     </div>
   )
 }
 
+function Countt (){
+  
+  const [count,setCount] = useState(0)
+
+  return <div>
+  {count}
+  <Increase setCount = {setCount}/>
+  <Decrease setCount = {setCount} />
+  </div>
+}
+
+
+function Increase ({setCount}){
+
+  function inc (){
+    setCount(c => c+1)
+  }
+
+  return <div>
+    <button onClick={inc}>Increase</button>
+  </div>
+}
+ 
+
+function Decrease ({setCount}){
+  function dec (){
+    setCount(c => c-1)
+  }
+
+  return <div>
+    <button onClick={dec}>Decrease</button>
+  </div>
+}
+ 
 export default Counterr
